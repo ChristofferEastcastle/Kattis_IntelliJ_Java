8802c0130a74bf3901ddf21c8958ec43d0d65e39
Christoffer Østborg <chos002@EGMS.no>
2021-02-23 12:50:35 +0100

    not finished

---
+++ src/Kleptography.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Kleptography {
+    public static void main(String[] args) {
+        char[] arr = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Character> alphabet = new ArrayList<>();
+        for (char c : arr)
+            alphabet.add(c);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+        char[] last = sc.nextLine().toCharArray();
+        char[] b = sc.nextLine().toCharArray();
+
+        for (int i = 0; i < b.length; i++) {
+            
+        }
+    }
+}
