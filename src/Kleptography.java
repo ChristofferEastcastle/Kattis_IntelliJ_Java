af682cf8dcacb5de8ecc9078911acf289135d97c
Christoffer Østborg <chos002@EGMS.no>
2021-02-25 16:53:43 +0100

    not finished

--- src/Kleptography.java
+++ src/Kleptography.java
@@ -10,11 +10,26 @@ public class Kleptography {
         int n = sc.nextInt();
         int m = sc.nextInt();
         sc.nextLine();
-        char[] last = sc.nextLine().toCharArray();
-        char[] b = sc.nextLine().toCharArray();
+        char[] last_n_plain = sc.nextLine().toCharArray();
 
+        char[] b = sc.nextLine().toCharArray();
+        ArrayList<Character> last = new ArrayList<>();
+        ArrayList<Character> cipherText = new ArrayList<>();
         for (int i = 0; i < b.length; i++) {
-            
+            if (i <= last_n_plain.length - 1)
+                last.add(last_n_plain[i]);
+            cipherText.add(b[i]);
         }
+        Collections.reverse(last);
+        Collections.reverse(cipherText);
+        ArrayList<Character> key_list = new ArrayList<>();
+
+        char[] key = new char[b.length];
+        for (int i = 0; i < n; i++){
+            key_list.add((char) (cipherText.get(i) - last.get(i) % 97));
+        }
+        System.out.println(last);
+        System.out.println(cipherText);
+        System.out.println(key_list);
     }
 }
