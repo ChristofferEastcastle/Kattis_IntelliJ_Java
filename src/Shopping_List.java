868e129d715132d111d7d78e27b8a4926ca2a5da
Christoffer <links685@gmail.com>
2021-03-04 22:31:33 +0100

    small changes so everything is in one loop

--- src/Shopping_List.java
+++ src/Shopping_List.java
@@ -6,6 +6,7 @@ public class Shopping_List {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
+        ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             String[] arr = sc.nextLine().split(" ");
             HashSet<String> set = new HashSet<>();
@@ -17,12 +18,9 @@ public class Shopping_List {
                 else
                     map.put(str, 1);
                 set.add(str);
+                if (map.get(str) == n)
+                    list.add(str);
             }
-        }
-        ArrayList<String> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()){
-            if (entry.getValue() == n)
-                list.add(entry.getKey());
         }
         System.out.println(list.size());
         Collections.sort(list);
